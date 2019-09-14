1e84ae7941e991d0f62fe38213dda5ae9f3e21f6
Jeffrey Cousineau <jeffrey.cousineau@gmail.com>
2019-09-14 16:19:36 -0400

    First mecanum wheels simulation test

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/jeffrey/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/jeffrey/dataset_ws/devel;/opt/ros/kinetic".split(';') if "/home/jeffrey/dataset_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jeffrey/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jeffrey/dataset_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64"
+export PATH="/opt/ros/kinetic/bin:/usr/local/cuda-9.0/bin:/home/jeffrey/bin:/home/jeffrey/Téléchargements/exec -l /bin/bash/google-cloud-sdk/bin:/home/jeffrey/bin:/home/jeffrey/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PWD="/home/jeffrey/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jeffrey/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jeffrey/catkin_ws/src:$ROS_PACKAGE_PATH"
